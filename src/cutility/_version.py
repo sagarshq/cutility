df552696f1a6ea8c0a6efb9d93fa9de6b608c5c7
Sagar Sarkale <sagarsarkale.work@gmail.com>
2024-09-07 13:30:21 +0530

    [add] workflow changes

--- src/cutility/_version.py
+++ src/cutility/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev30+g86ffb97.d20240907'
-__version_tuple__ = version_tuple = (0, 1, 'dev30', 'g86ffb97.d20240907')
+__version__ = version = '0.1.dev31+gbbab132.d20240907'
+__version_tuple__ = version_tuple = (0, 1, 'dev31', 'gbbab132.d20240907')
