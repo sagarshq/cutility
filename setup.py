7589296cceef3898689466ceca94e8b550bc3e5c
Sagar Sarkale <sagarsarkale.work@gmail.com>
2024-09-07 13:02:40 +0530

    [add] setup changes

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup, find_packages
+from setuptools_scm.version import get_local_node_and_date
 
 with open("README.md", "r", encoding="utf-8") as readme_file:
     long_description = readme_file.read()
 
+
+def local_scheme(version):
+    return ""
+
+
 setup(
     name="cutility",
-    use_scm_version=True,  # Use setuptools_scm for versioning
+    use_scm_version={
+        "local_scheme": local_scheme,  # Avoid local versions
+        "write_to": "src/cutility/_version.py",
+    },
     setup_requires=["setuptools>=42", "setuptools_scm"],
     author="Sagar Sarkale",
     author_email="sagarsarkale.work@gmail.com",
@@ -23,4 +32,10 @@ setup(
     packages=find_packages(where="src"),
     python_requires=">=3.7",
     install_requires=["simplejson", "pyyaml", "python-dotenv"],
+    # Optional: Add entry points if you have console scripts
+    # entry_points={
+    #     "console_scripts": [
+    #         "your_command=your_module:main_function",
+    #     ],
+    # },
 )
