bbab1322398df63c0ca4bbc08b3782a4fc73fdc6
Sagar Sarkale <sagarsarkale.work@gmail.com>
2024-09-07 13:26:54 +0530

    [add] workflow update

--- setup.py
+++ setup.py
@@ -1,26 +1,11 @@
 from setuptools import setup, find_packages
 
-# Dynamically install setuptools_scm if not available
-try:
-    from setuptools_scm import get_version
-except ImportError:
-    import subprocess
-
-    subprocess.check_call(["pip", "install", "setuptools_scm"])
-    from setuptools_scm import get_version
-
 with open("README.md", "r", encoding="utf-8") as readme_file:
     long_description = readme_file.read()
-
-
-def local_scheme(version):
-    return ""
-
 
 setup(
     name="cutility",
     use_scm_version={
-        "local_scheme": local_scheme,
         "write_to": "src/cutility/_version.py",
     },
     setup_requires=["setuptools>=42", "setuptools_scm"],
